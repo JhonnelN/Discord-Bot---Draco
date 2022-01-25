7a3e5579bb202dc9316e89ac7ec604118366fe7f
JhonnelN <45217131+JhonnelN@users.noreply.github.com>
2022-01-25 15:10:53 -0400

    Added Hidra
    Hidra Command _hidra

--- draco.py
+++ draco.py
@@ -4,6 +4,7 @@ from discord.ext import commands #importamos los comandos
 import datetime 
 import asyncio
 
+
 bot = commands.Bot(command_prefix='_', description="Wemix Draco Price Track")
 
 class Crypto_Price:
@@ -20,6 +21,10 @@ class Crypto_Price:
 # Get Price
 draco_wemix_price = Crypto_Price.get_price('https://api.mir4global.com/wallet/prices/draco/lastest' , 'USDDracoRate')
 "${0:3} USD".format(draco_wemix_price)
+
+hidra_wemix_price = Crypto_Price.get_price('https://api.mir4global.com/wallet/prices/hydra/lastest' , 'USDHydraRate')
+"${0:3} USD".format(hidra_wemix_price)
+
 # Discord
 # Bot commands
 @bot.command()
@@ -36,10 +41,26 @@ async def price(ctx):
      await ctx.send("${} USD Draco Oficial Web".format(Crypto_Price.get_price('https://api.mir4global.com/wallet/prices/draco/lastest' , 'USDDracoRate')))
      await ctx.send("${} USD Klay".format(Crypto_Price.get_price('https://api.mir4global.com/wallet/prices/draco/lastest' , 'USDKLAYRate')))
      await ctx.send("${} USD Wemix Credit".format(Crypto_Price.get_price('https://api.mir4global.com/wallet/prices/draco/lastest' , 'USDWemixRate')))
+     await ctx.send("${} USD Hidra".format(Crypto_Price.get_price('https://api.mir4global.com/wallet/prices/hydra/lastest' , 'USDHydraRate')))
 
 @bot.command()
 async def draco(ctx):
      await ctx.send("${} USD Draco".format(Crypto_Price.get_price('https://api.mir4global.com/wallet/prices/draco/lastest' , 'USDDracoRate')))
+
+@bot.command()
+async def hidra(ctx):
+     await ctx.send("${} USD Hidra".format(Crypto_Price.get_price('https://api.mir4global.com/wallet/prices/hydra/lastest' , 'USDHydraRate')))
+
+@bot.command()
+async def calculate(ctx,  amount = 0, coin = "draco"):
+     coin = coin.lower()
+     if coin == "draco":
+          await ctx.send("${0:3} USD".format(float((Crypto_Price.get_price('https://api.mir4global.com/wallet/prices/draco/lastest' , 'USDDracoRate')))*amount))
+     if amount == 150:
+          await ctx.send("Esal e viado")
+     if coin == "klay":
+          await ctx.send("${0:3} USD".format(float((Crypto_Price.get_price('https://api.mir4global.com/wallet/prices/draco/lastest' , 'USDKLAYRate')))*amount))
+
 
 
 # Show price in discord
@@ -62,11 +83,12 @@ async def on_ready():
                await asyncio.sleep(5)
                wemix_price = Crypto_Price.get_price('https://api.mir4global.com/wallet/prices/draco/lastest' , 'USDWemixRate')
                await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name= "${} USD|Wemix Credit".format(wemix_price)))
+               await asyncio.sleep(5)
+               hidra_price = Crypto_Price.get_price('https://api.mir4global.com/wallet/prices/hydra/lastest' , 'USDHydraRate')
+               await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name= "${} USD|Hidra".format(hidra_price)))
           except:
                pass
           
 
           # Security
-#bot.run('')
-
-
+bot.run('secret')
